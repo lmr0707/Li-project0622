3e547368a6ee18a49b1d556f47351f751817bf69
lmr0707 <564819384@qq.com>
2018-06-15 17:01:50 +0800

    js0615

---
+++ js/index-baiduyun.js
@@ -0,0 +1,46 @@
+//nav鼠标移入移除二级菜单效果
+$('.nav0').on({
+     'mouseenter':function () {
+         $('.myCon_1').stop().show();
+     },
+     'mouseleave':function () {
+         $('.myCon_1').stop().hide();
+     }
+     }
+ );
+ $('.nav3').on({
+         'mouseenter':function () {
+             $('.myCon_2').stop().show();
+         },
+         'mouseleave':function () {
+             $('.myCon_2').stop().hide();
+         }
+     }
+ );
+ $('.nav4').on({
+         'mouseenter':function () {
+             $('.myCon_3').stop().show();
+         },
+         'mouseleave':function () {
+             $('.myCon_3').stop().hide();
+         }
+     }
+ );
+$('.nav5').on({
+        'mouseenter':function () {
+            $('.myCon_4').stop().show();
+        },
+        'mouseleave':function () {
+            $('.myCon_4').stop().hide();
+        }
+    }
+);
+$('.nav6').on({
+        'mouseenter':function () {
+            $('.myCon_5').stop().show();
+        },
+        'mouseleave':function () {
+            $('.myCon_5').stop().hide();
+        }
+    }
+);
