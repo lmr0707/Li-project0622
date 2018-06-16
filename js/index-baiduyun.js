33c6790677370f6d782d301aa443803e6f28252f
lmr0707 <564819384@qq.com>
2018-06-16 19:21:18 +0800

    nav 1 finish

--- js/index-baiduyun.js
+++ js/index-baiduyun.js
@@ -44,3 +44,27 @@ $('.nav6').on({
         }
     }
 );
+//nav鼠标移入移除切换效果
+for(let i=0; i<12;i++){
+
+$('.nav-types>h3').eq(i).mouseenter(function () {
+    $('.nav-content>li').eq(i).show();
+    $('.nav-content>li').eq(i).siblings().hide();
+});
+}
+
+//nav鼠标移入背景颜色字体颜色改变，缩进
+$('.sub-nav .nav-group .nav-types h3').eq(0).css({'background':'#1B1B25 url(https://cdn.bdstatic.com/portal/img/common/icon_02901645.png) 200px -117px no-repeat',
+    'color': '#0b83e1',
+    'padding-left':'60px'
+});
+$('.sub-nav .nav-group .nav-types h3').mouseenter(function () {
+       $(this).css({'background':'#1B1B25 url(https://cdn.bdstatic.com/portal/img/common/icon_02901645.png) 200px -117px no-repeat',
+           'color': '#0b83e1'
+       });
+       $(this).stop().animate({'padding-left':'60px'},200);
+
+        $(this).siblings().css({'background':'none',
+            'color': '#999'});
+        $(this).siblings().stop().animate({'padding-left':'46px'},100);
+});
